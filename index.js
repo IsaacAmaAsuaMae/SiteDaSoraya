d0d37e3366a58402186d034a9bff65d5629acf8a
Soraya <sorayaklfranca@gmail.com>
2025-01-26 15:00:29 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function mostrar() {
+      document.getElementById("texto").style.display = "block";
+      document.getElementById("botao").style.display = "none";
+      document.getElementById("foto").style.display = "block";
+    }
 
+    function lembrete() {
+      alert("Lembre-se, eu te amo! E você é a melhor mãe do mundo.");
+    }
+
+    setInterval(lembrete, 10000);
