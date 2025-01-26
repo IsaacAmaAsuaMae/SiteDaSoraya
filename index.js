ef2671857fa2a7c951685da129893cbd5e7bb057
Soraya <sorayaklfranca@gmail.com>
2025-01-26 15:50:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 function mostrar() {
-      document.getElementById("texto").style.display = "block";
-      document.getElementById("botao").style.display = "none";
-      document.getElementById("foto").style.display = "block";
-      document.getElementById("paragrafo").style.display = "none";
-    }
+  document.getElementById("texto").style.display = "block";
+  document.getElementById("botao").style.display = "none";
+  document.getElementById("foto").style.display = "block";
+  document.getElementById("paragrafo").style.display = "none";
+}
 
-    function lembrete() {
-      alert("Lembre-se, eu te amo! E você é a melhor mãe do mundo.");
-    }
+function lembrete() {
+  alert("Lembre-se, eu te amo! E você é a melhor mãe do mundo.");
+}
 
-    setInterval(lembrete, 30000);
+window.onload = function() {
+  setInterval(lembrete, 30000);
+};
